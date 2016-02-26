a3dffaf28b8690b2984a4134ec1cdff40944e84f
treezrppl2 <gojiradestroy@gmail.com>
2016-02-26 10:23:30 -0800

    Add some mock-data to play with

---
+++ data.js
@@ -0,0 +1,56 @@
+var LATIN = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed \
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim \
+      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
+      aliquip ex ea commodo consequat. Duis aute irure dolor in \
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
+      culpa qui officia deserunt mollit anim id est laborum."
+// var Review = {                                     // [[Review Object]]
+//   Restaurant: /* referenceTo() */ Restaurant,        // [[Restaurant Object]]
+//   rating: 2.5 /* 1~5 */                              // rating as Integer
+//   body: "Lorem ipsum dolor sit amet..."              // body as String
+// }
+var Reviews = [
+  {
+    restaurantName: "Pho 111",
+    restaurantLocation: "111 Address Rd. City CA 90001",
+    rating: 5,
+    body: LATIN,
+    attachment: 'images/pho.jpg'
+  },
+  {
+    restaurantName: "Ionian Burger 2",
+    restaurantLocation: "2222 Address Rd. City CA 90002",
+    rating: 4,
+    body: LATIN,
+    attachment: 'images/burger.jpg'
+  },
+  {
+    restaurantName: "Tres Tacos",
+    restaurantLocation: "3333 Street Rd. Town CA 90003",
+    rating: 5,
+    body: LATIN,
+    attachment: 'images/taco.jpg'
+  },
+  {
+    restaurantName: "Bad Luck Sushi Four",
+    restaurantLocation: "44 Street Rd. Town CA 90004",
+    rating: 2,
+    body: LATIN,
+    attachment: 'images/sushi.jpg'
+  },
+  {
+    restaurantName: "Panera Bread #505",
+    restaurantLocation: "5505 Address Rd. City CA 90005",
+    rating: 4,
+    body: LATIN,
+    attachment: 'images/panini.jpg'
+  },
+  {
+    restaurantName: "Ionian Burger 2",
+    restaurantLocation: "2222 Address Rd. City CA 90002",
+    rating: 5,
+    body: LATIN,
+    attachment: 'images/burger2.jpg'
+  }
+]
