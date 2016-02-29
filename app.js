24199ec5dd4a24f7a6b7e649eab17434bfa6f078
treezrppl2 <gojiradestroy@gmail.com>
2016-02-29 15:09:16 -0800

    Added review.author to the output.

--- app.js
+++ app.js
@@ -25,41 +25,42 @@ function serveResults ( element, objects ) {
     //   images: ['path/to/images.jpg']
     // }
     var item = document.createElement('li');
-    item.className = 'media';
-    element.appendChild(item);
-
+      item.className = 'media';
+      element.appendChild(item);
     var mediaLeft = document.createElement('div');
-    mediaLeft.className = 'media-left';
-    item.appendChild(mediaLeft);
+      mediaLeft.className = 'media-left';
+      item.appendChild(mediaLeft);
     var imageWrapper = document.createElement('a');
-    imageWrapper.href = '#';
-    mediaLeft.appendChild(imageWrapper);
+      imageWrapper.href = '#';
+      mediaLeft.appendChild(imageWrapper);
     var image = document.createElement('img');
-    image.className = 'media-object'
-    image.src = obj.images[0];
-    imageWrapper.appendChild(image);
-
+      image.className = 'media-object'
+      image.src = obj.images[0];
+      imageWrapper.appendChild(image);
     var mediaBody = document.createElement('div');
-    mediaBody.className = 'media-body';
-    item.appendChild(mediaBody);
+      mediaBody.className = 'media-body';
+      item.appendChild(mediaBody);
     var name = document.createElement('h1');
-    name.className = 'media-heading';
-    mediaBody.appendChild(name);
-    name.appendChild(document.createTextNode(obj.name));
+      name.className = 'media-heading';
+      mediaBody.appendChild(name);
+      name.appendChild(document.createTextNode(obj.name));
     var rating = document.createElement('span');
-    rating.className = 'pull-right'
-    rating.appendChild(document.createTextNode(obj.reviews[0].rating+'★'));
-    name.appendChild(rating);
+      rating.className = 'rating'
+      rating.appendChild(document.createTextNode(' '+obj.reviews[0].rating+'★'));
+      name.appendChild(rating);
+    var author = document.createElement('span');
+      author.className = 'pull-right';
+      author.appendChild(document.createTextNode(obj.reviews[0].user));
+      name.appendChild(author)
     var review = document.createTextNode(obj.reviews[0].body)
-    mediaBody.appendChild(review);
+      mediaBody.appendChild(review);
     var tags = document.createElement('p');
-    tags.className = 'tags';
-    obj.tags.forEach(function(tag){
-      var tagElement = document.createElement('span');
-      tagElement.appendChild(document.createTextNode(tag+' '))
-      this.appendChild(tagElement);
-    }, tags);
-    mediaBody.appendChild(tags);
-
+      tags.className = 'tags';
+      obj.tags.forEach( function (tag){
+        var tagElement = document.createElement('span');
+        tagElement.appendChild(document.createTextNode(tag+' '))
+        this.appendChild(tagElement);
+      }, tags);
+      mediaBody.appendChild(tags);
   })
 }
