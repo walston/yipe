fb958defff9f55f435d76b6b0cf936f5ee032e30
treezrppl2 <gojiradestroy@gmail.com>
2016-03-07 12:22:43 -0800

    #9 User can now view a basic page for restaurants showing all related reviews.

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ var serveResults = function ( parent, restaurants ) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild)
   }
-  restaurants.forEach(function(restaurant) {
+  _.each(restaurants, function(restaurant, i) {
     restaurant.reviews.ideal = (function () {
       return _.max(restaurant.reviews, function (review) {
         return (review.ups['helpful'].length + review.ups['witty'].length);
@@ -47,19 +47,25 @@ var serveResults = function ( parent, restaurants ) {
         image.src = restaurant.images[0];
     var mediaBody = el('div', item, 'col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 col-md-10');
     var name = el('h1', mediaBody, 'h2');
-               txt(restaurant.name, name);
+    var nameLink = el('a', name)
+        nameLink.href = '#';
+        nameLink.addEventListener('click', function () {
+          serveLocation(roll, Restaurants[i]);
+        });
+               txt(restaurant.name, nameLink);
                txt(' ', name);
     var rating = el('span', name, 'text-muted h4');
-               txt(restaurant.rating+'☆', rating);
+               txt(restaurant.rating+'★', rating);
                txt(' ', name);
     var author = el('span', name, 'text-muted h4');
                txt(restaurant.reviews.ideal.user, author);
     var review = txt(restaurant.reviews.ideal.body, mediaBody);
     var tags = el('p', mediaBody);
-    restaurant.tags.forEach( function (tag, i){
-      var tagElement = el('span', this, 'text-info tag');
+    _.each(restaurant.tags, function (tag, i){
+      var tagElement = el('span', this, 'label label-default');
       txt(tag, tagElement)
       txt(' ', this);
+
     }, tags);
   });
   lastServed = restaurants;
@@ -81,16 +87,44 @@ var serveLocation = function ( parent, restaurant ) {
       function (s, j) { return s + j; }) / ratings.length);
     return average;
   })();
-  var backbutton = el('a', parent, 'btn btn-primary');
-    backbutton.href = '#';
-    backbutton.content = txt('←Back', backbutton)
-    backbutton.addEventListener('click', function(){ serveResults( roll, lastServed ); });
-  var plate = {};
-    plate.title = el('div', parent, 'row');
-    plate.title.name = el('span', plate.title, 'h2');
-    plate.title.name.text = txt(restaurant.name, plate.title.name);
-    plate.title.rating = el('span', plate.title, 'h2 info-text pull-right');
-    plate.title.rating.text = txt(restaurant.rating+'☆', plate.title.rating);
+  var localNav = el('div', parent, 'row em-bot');
+    localNav.backbutton = el('a', localNav, 'btn btn-primary');
+    localNav.backbutton.href = '#';
+    localNav.backbutton.content = txt('← Back', localNav.backbutton)
+    localNav.backbutton.addEventListener('click', function(){ serveResults( roll, lastServed ); });
+  var card = el('div', parent, 'row em-top');
+    card.head = el('div', card, 'col-xs-12');
+    card.head.name = el('h2', card.head);
+    card.head.name.text = txt(restaurant.name, card.head.name);
+    card.head.name.spacing = txt(' ', card.head.name);
+    card.head.rating = el('span', card.head.name, 'text-muted');
+    card.head.rating.text = txt(restaurant.rating+'★', card.head.rating);
+    card.head.info = el('div', card.head);
+    card.head.info.address = el('p', card.head.info, 'text-info');
+    card.head.info.address.text = txt(restaurant.address, card.head.info.address);
+    card.head.info.tags = el('p', card.head.info, 'en-top');
+    _.each(restaurant.tags, function (tag, i){
+      var tagElement = el('span', this, 'label label-default');
+      txt(tag, tagElement)
+      txt(' ', this);
+    }, card.head.info.tags);
+    card.head.imagery = el('div', card.head);
+    _.each(restaurant.images, function(path, i) {
+      var image = el('img', this, 'img-responsive thumbnail col-xs-2');
+       image.src = path;
+    }, card.head.imagery);
+    card.reviews = el('div', card, 'row en-top');
+    _.each(restaurant.reviews, function (review, i) {
+      var self = el('div', this, 'col-xs-12');
+      var info = el('div', self);
+        info.author = el('span', info, 'h4 text-primary');
+        info.author.text = txt(review.user, info.author);
+        info.spacing = txt(' ', info);
+        info.rating = el('span', info, 'h4 text-muted');
+        info.rating.text = txt(review.rating+'★', info.rating);
+      var body = el('div', self);
+       body.text = txt(review.body, body);
+    }, card.reviews);
 }
-serveLocation( roll, Restaurants[0] );
-// serveResults( roll, Restaurants );
+var serve =
+serveResults( roll, Restaurants );
